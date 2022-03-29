6c738bea4f0fb32055944e65e7b2b89962115ddc
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-03-29 12:11:24 -0400

    made a login section
    changed it to use bootstrap and styled it in my css file

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import "./Login.css"
+import Superfriends from './Superfriends';
+import Footer from './Footer';
 
 export default function Signin() {
     function handleSubmit(event) {
     event.preventDefault();
-    alert("login");
+    alert("Hello! You are now logged in");
   }
 
   return (
     <div className='Login'>
-    <h1>Sign in to your account</h1>
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="submit" value="Login" />
+    <div className="container"
+    >
+           <h1>Welcome Back</h1>
+      <h2 className='account mb-5'>please login to your Super Friends! account</h2>
+      <section>
+      <div className="row">
+        <div className='col'>
+      <Superfriends />
+      </div>
+      <div className='col'>
+
+    <form className="Form" onSubmit={handleSubmit}>
+      <input type="text" className="Username mb-3" placeholder="Username" />
+      <br />
+      <input type="password" className="Password mb-3" placeholder="Password" />
+      <br />
+      <input type="submit" className="Button w-100" value="Login" />
     </form>
+      </div>
+      </div>
+      </section>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
